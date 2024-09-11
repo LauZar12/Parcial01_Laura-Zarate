2e1dacaf7aca66a7f2647f287dcc485fb6fdd736
Laura Danniela Zárate Guerrero. <66334807+LauZar12@users.noreply.github.com>
2024-09-11 09:02:58 -0500

    last changes

--- Vuelos/models.py
+++ Vuelos/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Vuelo(models.Model):
     nacional = 'Nacional'
     internacional = 'Internacional'
-    tipo_vuelo= [
+    tipo_vuelo2= [
         (nacional, 'Nacional'),
         (internacional, 'Internacional'),
     ]
-    id = models.IntegerField(max_length=50)
+    id = models.IntegerField(max_length=50, primary_key=True)
     nombre = models.CharField(max_length=100)
-    tipo_vuelo = models.CharField(max_length=15, choices=tipo_vuelo)
+    tipo_vuelo = models.CharField(max_length=15, choices=tipo_vuelo2)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
